f48d3d6f77ec3376f6c75b6565dfba0e6c77775e
Susan Ashmore <susanashmore@Susans-MBP.teravim.com>
2025-07-01 20:49:35 -0400

    scraping from mygluten-freekitchen.com

--- recipescraper/spiders/mygf.py
+++ recipescraper/spiders/mygf.py
@@ -14,9 +14,48 @@ class MyGfSpider(scrapy.Spider):
                 yield response.follow(link, callback=self.parse_recipe)
 
     def parse_recipe(self, response):
+        title = response.css('h1.entry-title::text').get()
+        # Skip if not the proper structure for the recipe
+        if not response.css('div.mv-create-ingredients') and not response.css('div.mv-create-instructions'):
+            return
+
+        # Ingredients section
+        ingredients_sections = []
+        ingredients_div = response.css('div.mv-create-ingredients')
+        if ingredients_div:
+            current_section = {"section": "General", "items": []}
+            for el in ingredients_div.xpath("./*"):
+                if el.root.tag == "h4":
+                    # Save previous section if it has items
+                    if current_section["items"]:
+                        ingredients_sections.append(current_section)
+                    current_section = {"section": el.css("::text").get().strip(), "items": []}
+                elif el.root.tag == "ul":
+                    items = el.css("li::text").getall()
+                    current_section["items"].extend([i.strip() for i in items if i.strip()])
+            if current_section["items"]:
+                ingredients_sections.append(current_section)
+
+        # Instructions section
+        instructions_sections = []
+        instructions_div = response.css('div.mv-create-instructions')
+        if instructions_div:
+            current_section = {"section": "Instructions", "steps": []}
+            for el in instructions_div.xpath("./*"):
+                if el.root.tag == "h4":
+                    if current_section["steps"]:
+                        instructions_sections.append(current_section)
+                    current_section = {"section": el.css("::text").get().strip(), "steps": []}
+                elif el.root.tag == "ol":
+                    steps = el.css("li::text").getall()
+                    current_section["steps"].extend([s.strip() for s in steps if s.strip()])
+            if current_section["steps"]:
+                instructions_sections.append(current_section)
+
         yield {
-            'title': response.css('h1.entry-title::text').get(),
-            'ingredients': response.css('.wprm-recipe-ingredient-text::text').getall(),
-            'instructions': response.css('.wprm-recipe-instruction-text::text').getall(),
-            'url': response.url,
+            "title": title.strip() if title else None,
+            "ingredients": ingredients_sections,
+            "instructions": instructions_sections,
+            "url": response.url,
         }
+
